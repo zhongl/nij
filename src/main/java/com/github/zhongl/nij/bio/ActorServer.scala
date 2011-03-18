6c2bd1aafaced82921b18b46362c353c6f620330
zhongl <zhong.lunfu@gmail.com>
2011-03-18 11:31:14 +0800

    fix compile problem.

--- src/main/java/com/github/zhongl/nij/bio/ActorServer.scala
+++ src/main/java/com/github/zhongl/nij/bio/ActorServer.scala
@@ -9,22 +9,27 @@ import java.nio._
 object ActorServer {
   val RESPONSE: Array[Byte] = "HTTP/1.0 200 OK\r\nContent-Length:1\r\n\r\na".getBytes
   val BUFFER_IN: ByteBuffer = ByteBuffer.allocateDirect(1024)
-  val BUFFER_OUT: ByteBuffer = null
+  var BUFFER_OUT: ByteBuffer = null
 
-  def main(args: Seq[String]) {
-    val (host, port, backlog, size) = args
+  def main(args: Array[String]) {
+    println(args)
+    val Array(host, port, backlog, size) = args
     val socket: ServerSocket = new ServerSocket()
     socket.setReuseAddress(true);
     socket.setReceiveBufferSize(size.toInt * 1024)
     socket.setSoTimeout(500)
     socket.bind(new InetSocketAddress(host, port.toInt), backlog.toInt)
 
+    BUFFER_OUT = ByteBuffer.allocateDirect(RESPONSE.length)
+    BUFFER_OUT.put(RESPONSE)
+    BUFFER_OUT.flip
+
     while (true) {
       try {
         val accept: Socket = socket.accept()
         Actor.actor {
           accept.setKeepAlive(false)
-          accept.setSendBufferSize(size * 1024)
+          accept.setSendBufferSize(size.toInt * 1024)
           accept.setSoLinger(true, 1)
           accept.setTcpNoDelay(true)
           val readableByteChannel: ReadableByteChannel = Channels.newChannel(accept.getInputStream)
