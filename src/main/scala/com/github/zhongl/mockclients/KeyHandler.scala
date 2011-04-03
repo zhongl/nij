7cc1b88ab190f3111820658952ba8d57cf81b1fa
unknown <jushi@.taobao.ali.com>
2011-04-04 01:50:40 +0800

    change KeyHandler to interface.

--- src/main/scala/com/github/zhongl/mockclients/KeyHandler.scala
+++ src/main/scala/com/github/zhongl/mockclients/KeyHandler.scala
@@ -8,11 +8,11 @@ import java.nio.channels.SelectionKey
  * @author <a href="mailto:zhong.lunfu@gmail.com">zhongl</a>
  */
 trait KeyHandler {
-  def handleAcceptable(key: SelectionKey): Unit = {}
+  def handleAcceptable(key: SelectionKey): Unit
 
-  def handleConnectable(key: SelectionKey): Unit = {}
+  def handleConnectable(key: SelectionKey): Unit
 
-  def handleReadable(key: SelectionKey): Unit = {}
+  def handleReadable(key: SelectionKey): Unit
 
-  def handleWritable(key: SelectionKey): Unit = {}
+  def handleWritable(key: SelectionKey): Unit
 }
