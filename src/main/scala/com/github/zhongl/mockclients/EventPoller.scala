db7fdf49b40546e57f1c9e22df645cfd97ef9ce2
unknown <jushi@.taobao.ali.com>
2011-04-04 00:12:15 +0800

    use wakeupCalls intead inSelecting to avoid wakeup overload.

--- src/main/scala/com/github/zhongl/mockclients/EventPoller.scala
+++ src/main/scala/com/github/zhongl/mockclients/EventPoller.scala
@@ -5,7 +5,7 @@ import java.nio.channels.Selector.{open => newSelector}
 import scala.actors.Actor
 import scala.collection.JavaConversions._
 import com.github.zhongl.mockclients.Utils.{silent => silentCall}
-import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * {@link EventPoller} is a abstract class wrap a {@link Selector} to watching channels.
@@ -17,13 +17,14 @@ abstract class EventPoller(val timeout: Long) extends Logging {
   protected val selector = newSelector
   protected val worker = new Worker
 
-  private val inSelecting = new AtomicBoolean(false)
+  private val wakeupCalls = new AtomicInteger()
 
   final def start: Unit = worker.start ! Poll
 
-  final def stop: Unit = worker ! Exit(null)
+  final def stop: Unit = worker ! Exit(None)
 
-  protected def wakeup = if (inSelecting.compareAndSet(true, false)) selector.wakeup
+  /* To avoid wakeup overload, only first call can do wakeup the selector, and others wait until checking wakeupCalls. */
+  protected final def wakeup = if (wakeupCalls.incrementAndGet == 1) selector.wakeup
 
   protected def doHandle: PartialFunction[SelectionKey, Unit]
 
@@ -41,20 +42,19 @@ abstract class EventPoller(val timeout: Long) extends Logging {
     silentCall {key.channel.close}
   }
 
+  /* To wakeup selector only if more than once called wakeup, or else some wakeup events may be ignored. */
+  private def checkAndResetWakeupCalls: Unit = if (wakeupCalls.getAndSet(0) > 1) selector.wakeup
+
   private def hasSelectedKeys: Boolean = {
-    inSelecting.set(true)
-    try {
-      selector.select(timeout) > 0
-    } finally {
-      inSelecting.set(false)
-    }
+    checkAndResetWakeupCalls
+    selector.select(timeout) > 0
   }
 
   private def poll: Unit = {
     try {
       if (hasSelectedKeys) handleSelectedKeys
       worker ! Poll
-    } catch {case t => worker ! Exit(t)}
+    } catch {case t => worker ! Exit(Some(t))}
   }
 
   private def handleSelectedKeys: Unit = {
@@ -67,11 +67,12 @@ abstract class EventPoller(val timeout: Long) extends Logging {
       loop {
         react {
           case Poll => poll
-          case Exit(e) =>
+          case Exit(cause) =>
             if (selector.isOpen) silentCall {selector.close};
-            if (e != null) log.error("EventPoller exit with ", e)
+            if (cause.isEmpty) log.info("EventPoller exit normally.")
+            else log.error("EventPoller exit cause by ", cause.get)
             exit
-          case unknown: Command => extraExecute(unknown)
+          case extra: Command if extraExecute.isDefinedAt(extra) => extraExecute(extra)
           case illegal => log.error("Illegal command {}", illegal)
         }
       }
@@ -84,4 +85,4 @@ abstract class Command
 
 case class Poll extends Command
 
-case class Exit(throwable: Throwable) extends Command
+case class Exit(throwable: Option[Throwable]) extends Command
