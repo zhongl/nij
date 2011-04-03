4592737b2d8701890b9c6739111b1553b4e6522b
unknown <jushi@.taobao.ali.com>
2011-04-04 00:07:06 +0800

    provide more easier methods.

--- src/main/scala/com/github/zhongl/mockclients/Utils.scala
+++ src/main/scala/com/github/zhongl/mockclients/Utils.scala
@@ -1,15 +1,28 @@
 package com.github.zhongl.mockclients
 
 import scala.actors.Actor
-import java.nio.channels.{SocketChannel, SelectionKey}
+import java.nio.channels.{SelectionKey, SocketChannel}
+import java.nio.channels.SelectionKey._
 
 /**
  * @author <a href="mailto:zhong.lunfu@gmail.com">zhongl</a>
  */
 object Utils {
-  def uninterest(ops: Int, key: SelectionKey): Unit = key.interestOps(key.interestOps & ~ops)
+  def interestAccept(key: SelectionKey): Unit = interest(OP_ACCEPT, key)
 
-  def interest(ops: Int, key: SelectionKey): Unit = key.interestOps(key.interestOps | ops)
+  def interestConnect(key: SelectionKey): Unit = interest(OP_CONNECT, key)
+
+  def interestRead(key: SelectionKey): Unit = interest(OP_READ, key)
+
+  def interestWrite(key: SelectionKey): Unit = interest(OP_WRITE, key)
+
+  def uninterestAccept(key: SelectionKey): Unit = uninterest(OP_ACCEPT, key)
+
+  def uninterestConnect(key: SelectionKey): Unit = uninterest(OP_CONNECT, key)
+
+  def uninterestRead(key: SelectionKey): Unit = uninterest(OP_READ, key)
+
+  def uninterestWrite(key: SelectionKey): Unit = uninterest(OP_WRITE, key)
 
   def silent(call: => Unit): Unit = try {call} catch {case _ => Unit}
 
@@ -21,4 +34,8 @@ object Utils {
   }
 
   def socketChannelOf(key: SelectionKey): SocketChannel = key.channel.asInstanceOf[SocketChannel]
+
+  private def interest(ops: Int, key: SelectionKey): Unit = key.interestOps(key.interestOps | ops)
+
+  private def uninterest(ops: Int, key: SelectionKey): Unit = key.interestOps(key.interestOps & ~ops)
 }
